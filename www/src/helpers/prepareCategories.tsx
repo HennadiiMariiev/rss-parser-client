8c74ee7db0f7e6d088c46a1bd30c2ac7b14fd28b
Hennadii_Mariiev <marieff1990@gmail.com>
2022-07-28 13:33:52 +0300

    fix overlaytrigger

--- www/src/helpers/prepareCategories.tsx
+++ www/src/helpers/prepareCategories.tsx
@@ -1,14 +1,13 @@
 import React from 'react';
-import { OverlayTrigger, Tooltip, TooltipProps, Popover } from 'react-bootstrap';
+import { OverlayTrigger, Popover, PopoverProps } from 'react-bootstrap';
 
-import { isMobile } from './isMobile';
 import { ICategory } from '../interfaces/interfaces';
 import { MOBILE_SLICE_COUNT, DEFAULT_SLICE_COUNT } from '../../config/vars';
 
 import '../components/Posts/post.module.css';
 
 export function prepareCategories(categories: ICategory[] = [], isMobile: boolean) {
-  // const ref = useRef(null);
+  const SLICE_COUNT = isMobile ? MOBILE_SLICE_COUNT : DEFAULT_SLICE_COUNT;
 
   const cb = (item: ICategory, idx: number) => (
     <li key={idx} className="post-category">
@@ -16,45 +15,20 @@ export function prepareCategories(categories: ICategory[] = [], isMobile: boolea
     </li>
   );
 
-  const SLICE_COUNT = isMobile ? MOBILE_SLICE_COUNT : DEFAULT_SLICE_COUNT;
-
-  // const toolTip = (props: TooltipProps) => (
-  //   // <Tooltip id="category-tooltip" {...props} ref={ref}>
-  //   /* </Tooltip> */
-  //   // <Tooltip id="category-tooltip" {...props}>
-  //   //   {categories
-  //   //     .slice(SLICE_COUNT)
-  //   //     .map((item) => item?.name.toUpperCase())
-  //   //     .join(', ')}
-  //   // </Tooltip>
-  //   <Popover id="category-tooltip" {...props}>
-  //     <Popover.Header>Other categories</Popover.Header>
-  //     <Popover.Body>
-  //       {categories
-  //         .slice(SLICE_COUNT)
-  //         .map((item) => item?.name.toUpperCase())
-  //         .join(', ')}
-  //     </Popover.Body>
-  //   </Popover>
-  // );
-
-  const toolTip = (props: TooltipProps) => {
-    console.log(props);
-    return (
-      <Popover id="category-tooltip" {...props}>
-        <Popover.Header>Other categories</Popover.Header>
-        <Popover.Body>
-          {categories
-            .slice(SLICE_COUNT)
-            .map((item) => item?.name.toUpperCase())
-            .join(', ')}
-        </Popover.Body>
-      </Popover>
-    );
-  };
+  const popover = (props: PopoverProps) => (
+    <Popover id="category-tooltip" {...props}>
+      <Popover.Header className="popover-header">Other categories</Popover.Header>
+      <Popover.Body>
+        {categories
+          .slice(SLICE_COUNT)
+          .map((item) => item?.name.toUpperCase())
+          .join(', ')}
+      </Popover.Body>
+    </Popover>
+  );
 
   const othersEl = (
-    <OverlayTrigger key="category-tooltip" placement="bottom" overlay={toolTip}>
+    <OverlayTrigger key="category-tooltip" placement="bottom" overlay={popover}>
       <li className="post-category-others">
         <span className="post-category-text">AND {categories.length - SLICE_COUNT} OTHER...</span>
       </li>
