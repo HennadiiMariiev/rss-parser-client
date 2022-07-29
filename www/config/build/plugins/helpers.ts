5b42d1a3bbfb15a1cd7aa348d4aa127a650e076f
Hennadii_Mariiev <marieff1990@gmail.com>
2022-07-29 16:53:02 +0300

    [config] -> add meta:description, fix lazy loading

--- www/config/build/plugins/helpers.ts
+++ www/config/build/plugins/helpers.ts
@@ -81,6 +81,9 @@ export const renderHtml = ({ template, title, cssPath, jsPath, faviconPath, isPr
                 <meta name="viewport"
                       content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
                 <meta http-equiv="X-UA-Compatible" content="ie=edge">
+                <meta content="rss-parser-client.netlify.app" property="og:site_name">
+                <meta content="Lifehacker RSS parser" property="og:title">
+                <meta content="Lifehacker's RSS client application for viewing posts" property="og:description">
                 ${favicons}
                 <title>${title}</title>
                 ${cssLinks}
