ce627ce224307ed0124a283fa983a3c41b53f021
Hennadii_Mariiev <marieff1990@gmail.com>
2022-08-01 10:04:27 +0300

    fix noItems styles, fix pagination view and styles

--- www/src/components/Home.tsx
+++ www/src/components/Home.tsx
@@ -46,8 +46,11 @@ function Home() {
   const total = data?.data?.data?.pagination?.total;
 
   useEffect(() => {
-    if (total && total >= 0) {
-      setPagination((prev) => ({ ...prev, pageCount: Math.ceil(total / pagination.limit), total }));
+    if (typeof total === 'number') {
+      const totalValue = total > 0 ? total : 1;
+      setPagination((prev) => ({ ...prev, pageCount: Math.ceil(totalValue / pagination.limit), totalValue }));
+    } else {
+      setPagination((prev) => ({ ...prev, pageCount: 1, total: 0 }));
     }
   }, [total, pagination.limit, setPagination]);
 
