6c5c0a89b11fb5ef659a02db312a9c9077af95f7
Hennadii_Mariiev <gennadiy.mariev@devit.group>
2023-02-01 15:12:45 +0200

    [helpers] => add preparePagination f-tion

--- www/src/helpers/prepareParams.ts
+++ www/src/helpers/prepareParams.ts
@@ -2,11 +2,24 @@ import { IOptions } from '../interfaces/interfaces';
 
 const isNotEmpty = (obj: string[]) => !!Object.keys(obj).length;
 
+export function preparePaginationParams(options?: IOptions) {
+  const { page = 1, limit = 20 } = options ?? {};
+
+  return { page, limit };
+}
+
 export function prepareParams(options: IOptions) {
   const { creators, categories, search, ...rest } = options;
   const preparedCreators = isNotEmpty(creators) ? creators.join(',') : null;
-  const preparedCategories = isNotEmpty(categories) ? categories.join(',') : null;
+  const preparedCategories = isNotEmpty(categories)
+    ? categories.join(',')
+    : null;
   const preparedSearch = search ? { search } : null;
 
-  return { ...rest, creator: preparedCreators, categories: preparedCategories, search: preparedSearch };
+  return {
+    ...rest,
+    creator: preparedCreators,
+    categories: preparedCategories,
+    search: preparedSearch,
+  };
 }
